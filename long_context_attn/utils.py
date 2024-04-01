3a1573278013c222ff6b29245722368394ef769b
Jiarui Fang <fangjiarui123@gmail.com>
2024-04-01 16:52:49 +0800

    add use_ulysses_low flag (#28)

--- long_context_attn/utils.py
+++ long_context_attn/utils.py
@@ -9,22 +9,43 @@ from ring_flash_attn import (
 )
 
 
-def set_seq_parallel_pg(sp_ulysses_degree, sp_ring_degree, rank, world_size):
+def set_seq_parallel_pg(
+    sp_ulysses_degree, sp_ring_degree, rank, world_size, use_ulysses_low=True
+):
     num_ulysses_pgs = world_size // sp_ulysses_degree
-    for i in range(num_ulysses_pgs):
-        ulysses_ranks = list(range(i * sp_ulysses_degree, (i + 1) * sp_ulysses_degree))
-        group = torch.distributed.new_group(ulysses_ranks)
-        if rank in ulysses_ranks:
-            ulyssess_pg = group
-
     num_ring_pgs = world_size // sp_ring_degree
-    for i in range(num_ring_pgs):
-        ring_ranks = list(range(i, world_size, num_ring_pgs))
-        group = torch.distributed.new_group(ring_ranks)
-        if rank in ring_ranks:
-            ring_pg = group
 
-    return ulyssess_pg, ring_pg
+    if use_ulysses_low:
+        for i in range(num_ulysses_pgs):
+            ulysses_ranks = list(
+                range(i * sp_ulysses_degree, (i + 1) * sp_ulysses_degree)
+            )
+            group = torch.distributed.new_group(ulysses_ranks)
+            if rank in ulysses_ranks:
+                ulyssess_pg = group
+
+        for i in range(num_ring_pgs):
+            ring_ranks = list(range(i, world_size, num_ring_pgs))
+            group = torch.distributed.new_group(ring_ranks)
+            if rank in ring_ranks:
+                ring_pg = group
+
+        return ulyssess_pg, ring_pg
+
+    else:
+        for i in range(num_ring_pgs):
+            ring_ranks = list(range(i * sp_ring_degree, (i + 1) * sp_ring_degree))
+            group = torch.distributed.new_group(ring_ranks)
+            if rank in ring_ranks:
+                ring_pg = group
+
+        for i in range(num_ulysses_pgs):
+            ulysses_ranks = list(range(i, world_size, num_ulysses_pgs))
+            group = torch.distributed.new_group(ulysses_ranks)
+            if rank in ulysses_ranks:
+                ulyssess_pg = group
+
+        return ulyssess_pg, ring_pg
 
 
 RING_IMPL_DICT = {
