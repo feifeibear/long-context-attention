eaf58da7ff17abd6eef82a7b7f6cdc6cb5d44f88
feifeibear <fangjiarui123@gmail.com>
2024-04-01 03:35:54 +0000

    add ring-attn impl type

--- long_context_attn/utils.py
+++ long_context_attn/utils.py
@@ -1,4 +1,12 @@
 import torch
+from ring_flash_attn import (
+    ring_flash_attn_func,
+    ring_flash_attn_qkvpacked_func,
+    zigzag_ring_flash_attn_func,
+    zigzag_ring_flash_attn_qkvpacked_func,
+    stripe_flash_attn_func,
+    stripe_flash_attn_qkvpacked_func,
+)
 
 
 def set_seq_parallel_pg(sp_ulysses_degree, sp_ring_degree, rank, world_size):
@@ -17,3 +25,16 @@ def set_seq_parallel_pg(sp_ulysses_degree, sp_ring_degree, rank, world_size):
             ring_pg = group
 
     return ulyssess_pg, ring_pg
+
+
+RING_IMPL_DICT = {
+    "basic": ring_flash_attn_func,
+    "zigzag": zigzag_ring_flash_attn_func,
+    "strip": stripe_flash_attn_func,
+}
+
+RING_IMPL_QKVPACKED_DICT = {
+    "basic": ring_flash_attn_qkvpacked_func,
+    "zigzag": zigzag_ring_flash_attn_qkvpacked_func,
+    "strip": stripe_flash_attn_qkvpacked_func,
+}
