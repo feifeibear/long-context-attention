85c5ed585ff73438ea345085723d0acad3350df1
feifeibear <fangjiarui123@gmail.com>
2024-04-09 07:35:06 +0000

    update benchmark scripts

--- benchmark/benchmark_longctx.py
+++ benchmark/benchmark_longctx.py
@@ -7,10 +7,36 @@ import argparse
 parser = argparse.ArgumentParser(description="Process some integers.")
 
 parser.add_argument(
-    "--nheads", type=int, default=2, help="an integer for the accumulator"
+    "--ring_impl_type",
+    type=str,
+    default="basic",
+    choices=["basic", "zigzag", "strip"],
+    help="ring attn implementation type",
+)
+parser.add_argument("--nheads", type=int, default=2, help="head number")
+parser.add_argument("--batch_size", type=int, default=2, help="batch size")
+parser.add_argument(
+    "--fwd_only", action="store_true", help="benchmark forward pass only"
+)
+parser.add_argument(
+    "--use_ulysses_lowdim",
+    action="store_true",
+    default=True,
+    help="ulysses process group on low dimension",
+)
+parser.add_argument(
+    "--ulysses_degree",
+    type=int,
+    default=1,
+    help="ulysses attention sequence parallel degree",
 )
 
 args = parser.parse_args()
+
+
+
+def color_print(text):
+    print("\033[91m {}\033[00m".format(text))
 
 
 def benchmark(num_iter=100, forward_only=True, log=True):
@@ -40,11 +66,11 @@ def benchmark(num_iter=100, forward_only=True, log=True):
 
     dout = torch.randn(batch_size, seqlen, nheads, d, device=device, dtype=dtype)
 
-    sp_ulysses_degree = min(nheads, world_size)
+    sp_ulysses_degree = min(args.ulysses_degree, world_size)
     sp_ring_degree = world_size // sp_ulysses_degree
 
     ulysses_pg, ring_pg = set_seq_parallel_pg(
-        sp_ulysses_degree, sp_ring_degree, rank, world_size
+        sp_ulysses_degree, sp_ring_degree, rank, world_size, args.use_ulysses_lowdim
     )
 
     longctx_attn = LongContextAttention(ulysses_pg, ring_pg)
@@ -97,8 +123,13 @@ if __name__ == "__main__":
     dist.init_process_group("nccl")
     rank = dist.get_rank()
 
-    forward_only = False
+    forward_only = args.fwd_only
 
+    torch.cuda.empty_cache()
+    if rank == 0:
+        color_print(
+            f"# long context attention qkvpacked. ulysses_degree : {args.ulysses_degree} fwd_only {forward_only} use_ulysses_lowdim {args.use_ulysses_lowdim}"
+        )
     torch.cuda.empty_cache()
     benchmark(forward_only=forward_only, log=False)
     benchmark(forward_only=forward_only, log=True)
