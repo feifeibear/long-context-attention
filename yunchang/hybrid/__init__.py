5d8b2f3fcbc65a4c1fb43c47b984fd8b655d715e
Jiarui Fang <fangjiarui123@gmail.com>
2024-04-15 20:25:11 +0800

    initial async ulysses attn (#43)

--- yunchang/hybrid/__init__.py
+++ yunchang/hybrid/__init__.py
@@ -1,8 +1,11 @@
 from .attn_layer import LongContextAttention, LongContextAttentionQKVPacked
+from .async_attn_layer import AsyncLongContextAttention
 
 from .utils import RING_IMPL_QKVPACKED_DICT
+
 __all__ = [
     "LongContextAttention",
     "LongContextAttentionQKVPacked",
-    "RING_IMPL_QKVPACKED_DICT"
+    "RING_IMPL_QKVPACKED_DICT",
+    "AsyncLongContextAttention",
 ]
