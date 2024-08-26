b2ee80dd2378afd97bc54381fe437827da01196a
yiakwy-xpu-ml-framework-team <leiwang2@amd.com>
2024-08-26 06:04:33 +0000

    add rocm support
    - speed up compilation
    - add softcap support

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="yunchang",
     version="0.3",
@@ -7,6 +8,9 @@ setup(
     url="https://github.com/feifeibear/long-context-attention",
     packages=find_packages(exclude=['test', 'benchmark']),
     install_requires=[
-        'flash-attn>=2.6.0',
+        'ninja',
+        # ROCM support has been added to main branch since Jun [PR#1010](https://github.com/Dao-AILab/flash-attention/pull/1010);
+        # flash_attn-2.6.3+rocm62 for pytorch 2.3a
+        'flash_attn @ git+https://git@github.com/Dao-AILab/flash-attention.git',
     ],
 )
