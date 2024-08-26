5ec5f55ebb79956b1515e80c704be9cb5821fd17
Jinzhe Pan <eigensystem1318@gmail.com>
2024-08-26 19:04:36 +0800

    fix: version number & install requires

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="yunchang",
-    version="0.2",
+    version="0.3",
     author="Jiarui Fang, Zilin Zhu, Yang Yu",
     url="https://github.com/feifeibear/long-context-attention",
     packages=find_packages(exclude=['test', 'benchmark']),
     install_requires=[
-        'flash-attn',
+        'flash-attn>=2.6.0',
     ],
 )
