c202684f1d39b420f97d6694b882139ee73bc652
yiakwy-xpu-ml-framework-team <leiwang2@amd.com>
2024-08-27 09:45:09 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ setup(
     packages=find_packages(exclude=['test', 'benchmark']),
     install_requires=[
         'ninja',
-        # ROCM support has been added to main branch since Jun [PR#1010](https://github.com/Dao-AILab/flash-attention/pull/1010);
-        # flash_attn-2.6.3+rocm62 for pytorch 2.3a
-        'flash_attn @ git+https://git@github.com/Dao-AILab/flash-attention.git',
+        'flash-attn>=2.6.0', # default install cuda version,
     ],
+    extras_require={ 
+        'amd': [
+            'ninja',
+            'flash_attn @ git+https://git@github.com/Dao-AILab/flash-attention.git' # flash_attn-2.6.3+rocm62 for pytorch 2.3.0a0
+            ],
+    },
 )
