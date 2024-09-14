5ac27d8913167a97b10a84440936188d1d26a1d5
Jiarui Fang <fangjiarui123@gmail.com>
2024-09-14 11:30:33 +0800

    use extract_local for test_hybrid_attn.py (#74)

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+# 读取版本信息
+version_file = os.path.join(os.path.dirname(__file__), 'yunchang', '__version__.py')
+with open(version_file, 'r') as f:
+    exec(f.read())
 
 setup(
     name="yunchang",
-    version="0.3",
-    author="Jiarui Fang, Zilin Zhu, Yang Yu",
+    version=__version__,
+    author="fangjiarui123@gmail.com",
     url="https://github.com/feifeibear/long-context-attention",
     packages=find_packages(exclude=['test', 'benchmark']),
     install_requires=[
--- yunchang/comm/extract_local.py
+++ yunchang/comm/extract_local.py
@@ -7,23 +7,19 @@ from yunchang.globals import PROCESS_GROUP
 def stripe_extract_local(value, rank, world_size, rd, ud, *args, **kwargs):
     # ud at the highest dim
     input_dim = value.dim()
-    if input_dim == 5:
-        batch_size, seqlen, _, nheads, d = value.shape
-    elif input_dim == 4:
-        batch_size, seqlen, nheads, d = value.shape
-    else:
-        raise ValueError("value dim should be 4 or 5")
+    assert input_dim >= 2
 
-    # (ud, L, rd)
+    batch_size, seqlen, *rest = value.shape
+
+    assert dist.get_world_size(group=PROCESS_GROUP.RING_PG) == rd
+    assert dist.get_world_size(group=PROCESS_GROUP.ULYSSES_PG) == ud
+    
     value = value.reshape(batch_size, seqlen // rd, rd, -1).contiguous()
     value = value.transpose(1, 2).reshape(batch_size, seqlen, -1).contiguous()
     value = value.chunk(world_size, dim=1)[rank]
 
-    if input_dim == 5:
-        value = value.reshape(batch_size, seqlen // world_size, 3, nheads, d)
-    elif input_dim == 4:
-        value = value.reshape(batch_size, seqlen // world_size, nheads, d)
-    return value
+    new_shape = [batch_size, seqlen // world_size] + rest
+    return value.reshape(new_shape)
 
 
 def basic_extract_local(value, rank, world_size, *args, **kwargs):
@@ -31,32 +27,27 @@ def basic_extract_local(value, rank, world_size, *args, **kwargs):
 
 
 def zigzag_extract_local(value, rank, world_size, rd, ud, dim=1, *args, **kwargs):
+    """
+    value is a tensor of shape (bs, seqlen, ...)
+    """
     input_dim = value.dim()
-    if input_dim == 5:
-        batch_size, seqlen, _, nheads, d = value.shape
-    elif input_dim == 4:
-        batch_size, seqlen, nheads, d = value.shape
-    else:
-        raise ValueError("value dim should be 4 or 5")
+    assert input_dim >= 2
+    batch_size, seqlen, *rest = value.shape
 
     value_chunks = value.chunk(2 * rd, dim=dim)
-
-    # TODO assert ulyssess on low dim
     r_rank = dist.get_rank(group=PROCESS_GROUP.RING_PG)
     u_rank = dist.get_rank(group=PROCESS_GROUP.ULYSSES_PG)
+
+    assert dist.get_world_size(group=PROCESS_GROUP.RING_PG) == rd
+    assert dist.get_world_size(group=PROCESS_GROUP.ULYSSES_PG) == ud
 
     local_value = torch.cat(
         [value_chunks[r_rank], value_chunks[2 * rd - r_rank - 1]], dim=dim
     ).chunk(ud, dim=dim)[u_rank]
 
-    if input_dim == 5:
-        local_value = local_value.reshape(
-            batch_size, seqlen // world_size, 3, nheads, d
-        )
-    elif input_dim == 4:
-        local_value = local_value.reshape(batch_size, seqlen // world_size, nheads, d)
+    new_shape = [batch_size, seqlen // world_size] + rest
+    return local_value.reshape(new_shape).contiguous()
 
-    return local_value.contiguous()
 
 
 EXTRACT_FUNC_DICT = {
