9699e24d149c4d1969a7770f271315970350fc02
Jiarui Fang <fangjiarui123@gmail.com>
2024-09-14 12:13:09 +0800

    remove amd installation to an individual doc (#76)

--- setup.py
+++ setup.py
@@ -14,14 +14,5 @@ setup(
     packages=find_packages(exclude=['test', 'benchmark']),
     install_requires=[
         'flash-attn>=2.6.0', # default install cuda version,
-    ],
-    extras_require={ 
-        'amd': [
-            'flash_attn @ git+https://git@github.com/Dao-AILab/flash-attention.git', # flash_attn-2.6.3+rocm62 for pytorch 2.3.0a0
-            'einops'
-        ],
-    },
-    setup_requires=[
-        "ninja",
-    ],
+    ]
 )
