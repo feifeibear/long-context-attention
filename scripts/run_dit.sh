856fdd027dfb3aa9315846fc9ed6e6b4786f00cb
Jiarui Fang <fangjiarui123@gmail.com>
2024-11-15 21:19:40 +0800

    ulysses in benchmark (#104)

--- scripts/run_dit.sh
+++ scripts/run_dit.sh
@@ -1,5 +1,4 @@
-export PYTHONPATH=$PWD:$PYTHONPATH
-# export NCCL_PXN_DISABLE=1
+export PYTHONPATH=$PWD:$PYTHONPATH# export NCCL_PXN_DISABLE=1
 # export NCCL_DEBUG=INFO
 # export NCCL_SOCKET_IFNAME=eth0
 # export NCCL_IB_GID_INDEX=3
@@ -27,22 +26,23 @@ export GLOO_SOCKET_IFNAME=eth0
 # comment this line fwd+bwd
 # FWD_FLAG="--fwd_only"
 
-NHEADS=72
-SEQLEN=131072
+NHEADS=24
+SEQLEN=1024
 GROUP_NUM=1
-GPU_NUM=8
+GPU_NUM=2
+HEAD_SIZE=128
 ULYSSES_DEGREE=8
 
 NRANK=${NRANK:-0}
 # RING_IMPL_TYPE="zigzag"
 
 # make sure NHEADS // GROUP_NUM % ULYSSES_DEGREE == 0
-for attn_type in "fa" "fa3"; do
-for ULYSSES_DEGREE in 8 4 2 1; do
+for attn_type in "torch" "fa" "fa3"; do
+for ULYSSES_DEGREE in 2; do
 for RING_IMPL_TYPE in "basic"; do
 torchrun --nproc_per_node $GPU_NUM --node_rank $NRANK benchmark/benchmark_longctx.py \
 --nheads $NHEADS --group_num $GROUP_NUM --batch_size 1 $FWD_FLAG --seq_len $SEQLEN --head_size $HEAD_SIZE \
---ulysses_degree $ULYSSES_DEGREE --ring_impl_type $RING_IMPL_TYPE --no_causal --attn_type $attn_type
+--ulysses_degree $ULYSSES_DEGREE --ring_impl_type $RING_IMPL_TYPE --no_causal --attn_type $attn_type --use_ulysses
 done
 done
 done
