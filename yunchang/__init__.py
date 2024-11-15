bcfd6f07711219c0920d61bd0197a91a313dd6eb
feifeibear <fangjiarui123@gmail.com>
2024-11-15 16:05:14 +0800

    move version to __init__.py

--- yunchang/__init__.py
+++ yunchang/__init__.py
@@ -3,3 +3,6 @@ from .ring import *
 from .ulysses import *
 from .globals import set_seq_parallel_pg
 from .comm.extract_local import stripe_extract_local, basic_extract_local, zigzag_extract_local, EXTRACT_FUNC_DICT
+
+__version__ = "0.4.0"
+
