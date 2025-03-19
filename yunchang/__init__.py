1154a1d3f7806f4109bd490c82a80dc44613c9ad
feifeibear <fangjiarui123@gmail.com>
2025-03-19 13:51:56 +0000

    bump to version 0.6.1

--- yunchang/__init__.py
+++ yunchang/__init__.py
@@ -4,5 +4,5 @@ from .ulysses import *
 from .globals import set_seq_parallel_pg
 from .comm.extract_local import stripe_extract_local, basic_extract_local, zigzag_extract_local, EXTRACT_FUNC_DICT
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
