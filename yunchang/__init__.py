83d7b4e48cd68287cf33451ca38ec4aae367094c
Jiarui Fang（方佳瑞） <fangjiarui123@gmail.com>
2025-04-18 13:39:24 +0800

    bump to version 0.6.3 (#144)

--- yunchang/__init__.py
+++ yunchang/__init__.py
@@ -2,7 +2,11 @@ from .hybrid import *
 from .ring import *
 from .ulysses import *
 from .globals import set_seq_parallel_pg
-from .comm.extract_local import stripe_extract_local, basic_extract_local, zigzag_extract_local, EXTRACT_FUNC_DICT
+from .comm.extract_local import (
+    stripe_extract_local,
+    basic_extract_local,
+    zigzag_extract_local,
+    EXTRACT_FUNC_DICT,
+)
 
-__version__ = "0.6.2"
-
+__version__ = "0.6.3"
